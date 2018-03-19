4f1a589b3213c9634e0df1df78e963ec7e164f93
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-03-19 16:50:43 +0300

    Add changes to button names

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -10,12 +10,7 @@ export class VoteComponent implements OnInit {
 
   @Output() isComplete = new EventEmitter<boolean>();
 
-  quotes= [
-    new Quote(1,'We accept the love we think we deserve', 'Stephen Chbosky',new Date(2016,3,14)),
-    new Quote(2,'Without music, life would be a mistake', 'Friedrich Nietzsche',new Date(2015,3,14)),
-    new Quote(3,'Always forgive your enemies; nothing annoys them so much', 'Oscar Wilde',new Date(2017,3,14)),
-    new Quote(4, 'A friend is someone who knows all about you and still loves you','Elbert Hubbard',new Date(2013,3,14)),
-  ];
+
 
   uvotes = 0;
   dvotes = 0;
